74a08b5d768c071e60442e45df080e0d1b103949
glavmonter <glavmonter@gmail.com>
2025-11-14 06:55:09 +0300

    ParkingEntry.sln

--- ParkingGate/ParkingGate.Domain/Commands/BarrierCommands.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-using ParkingGate.Domain.Models;
-
-namespace ParkingGate.Domain.Commands;
-
-public record OpenBarrierCommand : IRequest<BarrierStatus>;
-
-public record CloseBarrierCommand : IRequest<BarrierStatus>;
